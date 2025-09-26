6c43a8b4cfb2ad8183c60d203948427fcfdb55fb
geradev66 <geradev66@gmail.com>
2025-09-25 19:52:21 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const titulo = ref<string>('Proyecto de vue con typescript')
+
+
+</script>
+
+<template>
+  <h1>{{titulo}}</h1>
+</template>
+
